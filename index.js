32122471a8b3fb9c96539c0331cab924c797d9a7
Gen Hames <hamesg@tutanota.de>
2020-10-23 10:51:20 -0700

    Example

---
+++ index.js
@@ -0,0 +1,16 @@
+// From https://www.npmjs.com/package/logform#combining-formats
+
+const { format } = require('./logform.js');
+const { combine, timestamp, label } = format;
+
+const labelTimestamp = combine(
+  label({ label: 'right meow!' }),
+  timestamp()
+);
+
+const info = labelTimestamp.transform({
+  level: 'info',
+  message: 'What time is the testing at?'
+});
+
+console.dir(info);
